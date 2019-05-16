32e6177e9862e8cab9a3497e3afa1e282b25ebdc
Xuemin Guan <Xuemin Guan>
2019-05-16 19:25:45 +0100

    added a test for the sample date. This is going to take a while to pass

--- src/test/kotlin/daily/coding/problem/kotlin/day1/FindTwoNumbersAddUpToKTest.kt
+++ src/test/kotlin/daily/coding/problem/kotlin/day1/FindTwoNumbersAddUpToKTest.kt
@@ -1,8 +1,10 @@
 package daily.coding.problem.kotlin.day1
 
 import kotlin.random.Random
+import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class FindTwoNumbersAddUpToKTest {
     @Test fun `should always be false, ie can never find two numbers adding up to k,  when the list is empty`() {
@@ -14,5 +16,9 @@ class FindTwoNumbersAddUpToKTest {
         val i = Random(System.currentTimeMillis()).nextInt()
         val k = Random(System.currentTimeMillis()).nextInt()
         assertFalse(FindTwoNumbersAddUpToK(listOf(i), k))
+    }
+
+    @Ignore fun `should be true for the sample list in the problem statement`() {
+        assertTrue(FindTwoNumbersAddUpToK(listOf(10, 15, 3, 7), 17))
     }
 }
