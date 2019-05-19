7db80c8e6ac68b979d0917e5d3856f35f7ba635c
Xuemin Guan <Xuemin Guan>
2019-05-19 17:49:45 +0100

    day 3 | serialize and deserialize a binary tree | used a different algorithm to render the tree. Going left deep down first

--- src/main/kotlin/daily/coding/problem/day3/SerializeAndDeserializeBinaryTree.kt
+++ src/main/kotlin/daily/coding/problem/day3/SerializeAndDeserializeBinaryTree.kt
@@ -9,17 +9,23 @@ The form of the serialization chosen for the algorithm
     / \
    4   5
 
-as "[1,2,3,null,null,4,5]"
+as "[1,2,null,null,3,4,null,null,5,null,null]"
 
 */
 data class Node(val value: String, val left: Node?, val right: Node?)
 
 fun Node.serialize(): String {
-    return serialize(serialize(listOf(this.value, left?.value, right?.value), this.left), this.right).joinToString(",")
+    return serialize(this, listOf(this.value)).joinToString(",")
 }
 
-fun serialize(nodesSerializedSoFar: List<String?>, left: Node?): List<String?> {
-    return left?.let {
-        nodesSerializedSoFar.plus(it.left?.value).plus(it.right?.value)
-    }?:nodesSerializedSoFar
+private fun serialize(node: Node, nodesRenderSoFar: List<String?>): List<String?> {
+    val nullString: String? = null
+    val nodesSoFarIncludingLeftNodes = node.left?.let {
+        serialize(it, nodesRenderSoFar.plus(it.value))
+    } ?: nodesRenderSoFar.plus(nullString)
+
+    return node.right?.let {
+        serialize(it, nodesSoFarIncludingLeftNodes.plus(it.value))
+    } ?: nodesSoFarIncludingLeftNodes.plus(nullString)
 }
+
--- src/test/kotlin/daily/coding/problem/day3/SerializeAndDeserializeBinaryTreeTest.kt
+++ src/test/kotlin/daily/coding/problem/day3/SerializeAndDeserializeBinaryTreeTest.kt
@@ -29,7 +29,30 @@ class SerializeAndDeserializeBinaryTreeTest {
     fun `should serialize a binary tree - a root with one single right child and one single left child node`() {
         val rootWithOneSingleRightChildBinaryTree = Node("root", Node("left", null, null), Node("right", null, null))
 
-        assertThat(rootWithOneSingleRightChildBinaryTree.serialize()).isEqualTo("root,left,right,null,null,null,null")
+        assertThat(rootWithOneSingleRightChildBinaryTree.serialize()).isEqualTo("root,left,null,null,right,null,null")
+    }
+
+    @Test
+    fun `should serialize a binary tree - a root with one left child and one left grand child`() {
+        val rootWithOneLeftChildAndOneLeftGrandChild = Node("root",
+                Node("left", Node("left.left", null, null), null), null)
+
+        assertThat(rootWithOneLeftChildAndOneLeftGrandChild.serialize()).isEqualTo("root,left,left.left,null,null,null,null")
+    }
+
+    @Test
+    fun `should serialize a binary tree - a root with left child and one left grand child, one right grand child`() {
+        val rootWithOneLeftChildAndOneLeftGrandChildOneRightGrandChild = Node("root",
+                Node("left", Node("left.left", null, null), Node("left.right", null,null)), null)
+
+        assertThat(rootWithOneLeftChildAndOneLeftGrandChildOneRightGrandChild.serialize()).isEqualTo("root,left,left.left,null,null,null,null")
+    }
+
+    @Test
+    fun `should serialize a binary tree - a root with one level left child, and 2 level of right children - sample data`() {
+        val rootWithOneSingleRightChildBinaryTree =
+                Node("1", Node("2", null, null), Node("3", Node("4", null, null), Node("5", null, null)))
+
+        assertThat(rootWithOneSingleRightChildBinaryTree.serialize()).isEqualTo("1,2,null,null,3,4,null,null,5,null,null")
     }
 }
-
